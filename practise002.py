52963dad7fed3da270911d87857d4423cb96cd15
shaik asef <115339758+asefshaik@users.noreply.github.com>
2022-10-09 11:43:01 +0530

    Add files via upload

---
+++ practise002.py
@@ -0,0 +1,25 @@
+#STRINGS
+
+x = "thor odinson"
+print(x.upper())
+print(x.lower())
+
+x = "eat my hammer"
+print(x.strip())
+
+x = "iam loki of asgard"
+print(x.replace("k", "f"))
+
+x= "doctor strange"
+print(x.split())
+
+x= "doctor strange"
+y= x.split()
+print(y[1])
+
+x = "loki"
+y = "laufison"
+z = x+ " " +y
+print(z)
+
+
