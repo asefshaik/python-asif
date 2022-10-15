dc189b5f880163f3e3f8954ff85907b2ab764357
shaik asef <115339758+asefshaik@users.noreply.github.com>
2022-10-15 13:22:48 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,65 @@
+#pyton operators
+
+#arithmetic operator
+
+print(10+20) #addition
+print(20-10) #substraction
+print(10*20) #multiplication
+print(20/10) #division
+print(20%10) #gives remainder
+print(3**4)  #(**)means exponent
+
+print(15//2) #it will be 7.5, but gives nearest integer
+
+
+#ASSIGNMENT OPERATORS
+
+a = 20 # =
+
+a -=5 #same as a= a-5
+print(a)
+
+a *=5 #same as a= a*5
+print(a)
+
+a +=5 #(same as a = a+5)
+print(a)
+
+a /=5 # (same as a = a/5)
+print(a)
+
+#comparision
+
+print(3 == 4)
+
+print(3!= 4)
+
+print(3>4)
+
+print(3<4)
+
+#logical operators
+
+x=2
+print(x<5 and x<8) #it will be true if both statements are true
+
+print(x<5 and x>8) #it will be false if one of the statement is false
+
+print (x<5 or x>8) #it will be true if one of the statement is tuue(or)
+
+#identity operators
+
+a= [1,2]
+b= [1,2]
+c=a
+
+#print (a is c)
+
+print(a)
+print(b)
+print(c)
+
+
+
+
+
