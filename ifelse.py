751dba489f7410ede19f61dc5cdf87544f82effd
shaik asef <115339758+asefshaik@users.noreply.github.com>
2022-10-29 13:24:49 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,38 @@
+#if else conditions
+
+#if(5 >1):
+#    print ("five greater than one")
+
+#if(2>9):
+ #   print("hello bro")
+#elif(2<9):
+ #   print("wassup bro")
+#else:
+ #   print("bye bro")
+ 
+     
+# x = 40
+# y = 20
+# z = 30
+
+# if x > y and x > z:
+#     print("both the conditions are true")
+
+# if(x < y or x < z):
+#     print('If any one of the condition is true')
+
+     
+     
+#      age = 18
+
+# if( age < 18):
+#     print('age is less than 18')
+#     if(age < 10):
+#         print('the person is a minor. no entry allowed')
+#     else:
+#         print('the person is allowed') #range for this else is 10-17
+# else:
+#     print('age is greater than or equal to 18')
+
+
+
